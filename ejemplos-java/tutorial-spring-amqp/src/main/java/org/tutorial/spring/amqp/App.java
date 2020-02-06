de43419a50272792ae3052cae617d51722a8b6e3
trilo-bytes <alcarazdearcos@gmail.com>
2020-02-06 17:04:22 +0100

    Añadido tutorial rabbitmq

---
+++ ejemplos-java/tutorial-spring-amqp/src/main/java/org/tutorial/spring/amqp/App.java
@@ -0,0 +1,13 @@
+package org.tutorial.spring.amqp;
+
+/**
+ * Tutorial https://www.rabbitmq.com/tutorials/tutorial-one-spring-amqp.html
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
